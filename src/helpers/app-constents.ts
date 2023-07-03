815549a7b52b5edbe3922d3f0a0f1123cce00071
Senguly <senguly710@gmail.com>
2023-07-04 02:52:14 +0300

    remove old version API and update the country backend

--- src/helpers/app-constents.ts
+++ src/helpers/app-constents.ts
@@ -1,5 +1,4 @@
 export const appConst = {
-    BASE_URL0:'https://uniranks.com/ranking/api',
     BASE_URL:'https://uniranks.com/ranking/api/app/v2',
     GOOGLE_API_KEY:'UR.EzggrCaZatRnSxQ91FSs7ufqYolUE4664Snd5Woki1wCPtMQrzmXq',
     ROLE_UNIVERSITY_REPRESENTATIVE: 9,
--- src/shared/countries.ts
+++ src/shared/countries.ts
@@ -1,13 +1,13 @@
 import {onBeforeMount, ref} from "vue";
 import {appConst, fetchWrapper} from "@/helpers";
-const {BASE_URL0} = appConst;
+const {BASE_URL} = appConst;
 
 export function useCountries(){
     const countries = ref([]);
     const loading_countries = ref(false);
     const loadCountries = async ()=> {
         loading_countries.value  = true;
-        await fetchWrapper.get(`${BASE_URL0}/countries`)
+        await fetchWrapper.get(`${BASE_URL}/dialCodes`)
             .then((response)=> {
                 loading_countries.value = false;
                 return response.json()
